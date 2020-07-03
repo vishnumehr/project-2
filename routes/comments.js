44648aa6964f416c49bb960348b25dd321dee1a7
vishnumehr <66519881+vishnumehr@users.noreply.github.com>
2020-07-03 18:25:45 +0530

    Add files via upload

---
+++ routes/comments.js
@@ -0,0 +1,80 @@
+var express    = require("express");
+var router     = express.Router({mergeParams: true});
+var Campground = require("../models/campgrounds");
+var Comment    = require("../models/comment");
+
+//COMMENTS NEW
+
+router.get("/new", isLoggedIn, function(req, res){
+  //FIND CAMPGROUND BY ID
+  console.log(req.params.id);
+  Campground.findById(req.params.id, function(err, campground){
+    if(err){
+      console.log(err);
+    } else {
+       res.render("comments/new", {campground: campground});
+    }
+  })
+});
+
+//COMMENTS CREATE
+router.post("/",isLoggedIn,function(req, res){
+   //LOOKUP CAMPGROUND USING ID
+   Campground.findById(req.params.id, function(err, campground) {
+       if(err){
+         console.log(err);
+         res.redircet("/campgrounds");
+       } else {
+         console.log(req.body.Comment);
+        Comment.create(req.body.comment, function(err,comment){
+          if(err){
+            console.log(err);
+          } else {
+            //ADD USERNAME AND ID TO  COMMENT
+            comment.author.id = req.user._id;
+            comment.author.username = req.user.username;
+            //SAVE COMMENT
+            comment.save();
+            campground.comments.push(comment)
+            campground.save();
+            console.log(comment);
+            res.redirect('/campgrounds/' + campground._id);
+          }
+        });
+       }
+   });
+});
+
+// COMMENT EDIT ROUTE
+router.get("/:comment_id/edit", function(req, res){
+  Comment.findById(req.params.comment_id, function(err, foundComment){
+    if(err){
+      res.redirect("back");
+    } else {
+     res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+    }
+  });
+});
+
+// COMMENT UPDATE
+router.put("/:comment_id", function(req, res){
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+    if(err){
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id );
+    }
+  });
+});
+
+
+//MIDDLEWARE
+function  isLoggedIn(req, res, next){
+  if (req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
+
+
+module.exports = router;
